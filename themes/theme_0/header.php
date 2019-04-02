ab763dc739fb8b5578841577fcf81272178e4712
Juaneccle <jecclefieldg@udd.cl>
2019-04-02 20:51:17 -0300

    Avance nro 2

--- themes/theme_0/header.php
+++ themes/theme_0/header.php
@@ -32,6 +32,30 @@
     wp_head();
   ?>
 
+
+  <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="assets/img/axidraw.png" class="d-block w-100">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/img/axidraw.png" class="d-block w-100">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/img/axidraw.png" class="d-block w-100">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
 </head>
 
 <body>
